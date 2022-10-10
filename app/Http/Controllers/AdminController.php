74b0556ba326b91c3d174f4369aa0f66c79dd62d
088mickwilli <630112418088@bru.ac.th>
2022-10-10 16:29:42 +0700

    admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class AdminController extends Controller
 {

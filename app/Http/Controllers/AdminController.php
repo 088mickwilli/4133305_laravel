8a4b104842b640a21d59de5235f6a63980d7e6d0
088mickwilli <630112418088@bru.ac.th>
2022-10-10 17:31:36 +0700

    blade_register

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Session\Session;
+use app\Models\User;
 
 class AdminController extends Controller
 {
     public function register(){
-        return "Register";
+        return view('admin.register');
     }
 
     public function login(){
-        return "Login";
+        return view('admin.login');
     }
 
 }
---
+++ resources/views/admin/register.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <h3>Register</h3>
+                <form action="" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="name">ชื่อ-นามสกุล</label>
+                        <input type="text" name="name" class="form-control" placeholder="กรุณากรอกชื่อ" value="{{old('name')}}">
+                        <span class="text-danger">
+                            @error('name')
+                                {{$message}}
+                            @enderror
+
+                        </span>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">E-mail</label>
+                        <input type="email" name="email" class="form-control" placeholder="กรุณากรอกอีเมล์" value="{{old('email')}}">
+                        <span class="text-danger">
+                            @error('email')
+                                {{$message}}
+                            @enderror
+
+                        </span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">รหัสผ่าน</label>
+                        <input type="password" name="password" class="form-control" placeholder="กรุณากรอกรหัสผ่าน" value="{{old('password')}}">
+                        <span class="text-danger">
+                            @error('password')
+                                {{$message}}
+                            @enderror
+
+                        </span>
+                    </div>
+
+                    <br>
+                    <div class="form-group">
+                        <button class="btn btn-block btn-success">สมัครสมาชิก</button>
+                    </div>
+                    <br>
+                    <a href="{{url('login')}}">กลับไปหน้าล็อคอิน</a>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>

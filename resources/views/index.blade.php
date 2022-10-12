bee81401b6cc143e5572a30f6fb45c270cbf0bef
088mickwilli <630112418088@bru.ac.th>
2022-10-12 12:41:55 +0700

    center

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,8 +6,8 @@
         <div class="row gx-4 gx-lg-5 justify-content-center">
             <div class="col-md-10 col-lg-8 col-xl-7">
                 <div class="site-heading">
-                    <h1>Welcome</h1>
-                    <span class="subheading">หนังสือ</span>
+                    <center><h1>Welcome</h1></center>
+                    <center><span class="subheading">libary</span></center>
                 </div>
             </div>
         </div>
